b2ac59a7071e2790849a983f5d4ea86132a1f973
Nasim Ahmadi Ramezanlou <nasim.ahmadi@rasha.io>
2024-10-26 15:51:48 +0330

    redactor service repo query

--- src/main/java/com/example/demo/repository/ServiceRepository.java
+++ src/main/java/com/example/demo/repository/ServiceRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 @Repository
 public interface ServiceRepository extends JpaRepository<ServiceEntity, Long> {
-    @Query(value = "SELECT * FROM service_entity WHERE is_active = true", nativeQuery = true)
+    @Query(value = "SELECT s FROM ServiceEntity s WHERE s.isActive = true")
     List<ServiceEntity> findActiveServices();
 }
